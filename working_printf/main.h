5abfc63a5a22034b9eef36b23413d5c1a8176ab0
Zu50 <zohoureid44@gmail.com>
2023-07-21 18:21:52 -0700

    trying

---
+++ working_printf/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define UNUSED(y) (void)(y)
+#define BUFF_SIZE 1024
+
+/*Just a trail for now */
+#endif
